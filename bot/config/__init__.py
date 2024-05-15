c5b80044715522c329af976b5e36e55a0cfcc1a2
Asadbek <156520786+mexmonjonovuz@users.noreply.github.com>
2024-05-15 14:26:46 +0500

    Update __init__.py

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -1,6 +1,6 @@
 from redis_dict import RedisDict
 
-ADMIN_LIST = [5684649553]
-TOKEN = '7110743901:AAG-7F9BVztGrv_AeU0edxDbbnLsxMkEi5E'
+ADMIN_LIST = [] #admin ID
+TOKEN = '' #TOKEN
 db = RedisDict('all')
 print(db)
