f133f7342ed28715d454f941a924482ccb17f044
mexmonjonovuz <asadbekmehmonjonov89@gmail.com>
2024-11-17 17:13:08 +0500

    fix bags

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,29 +1,19 @@
 from aiogram import Dispatcher, Bot
 from aiogram.types import BotCommand
-from aiogram.utils.i18n import gettext as _, I18n
-
 from bot.config import db
 
-i18n = I18n(domain='messages', path='locales')
-
-_ = i18n.gettext
-
 dispatcher = Dispatcher()
-dispatcher['i18n'] = i18n
-
 
 async def on_startup(dispatcher: Dispatcher, bot: Bot):
-    if not (db.get('categories')):
+    if not db.get('categories'):
         db['categories'] = {}
     if not db.get('products'):
         db['products'] = {}
 
-    dispatcher['i18n'] = i18n
-
     command_list = [
-        BotCommand(command='start', description=_('Botni ishga tushirish 🫡')),
-        BotCommand(command='help', description=_('Yordam 📖')),
-        BotCommand(command='language', description=_('Tilni almashtirish 🔄'))
+        BotCommand(command='start', description='Start the bot 🫡'),
+        BotCommand(command='help', description='Help 📖'),
+        BotCommand(command='language', description='Change language 🔄')
     ]
     await bot.set_my_commands(command_list)
 
