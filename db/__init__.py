85790922c456375ba5fcc156fdd797d927635c82
mexmonjonovuz <asadbekmehmonjonov89@gmail.com>
2024-12-06 12:55:15 +0500

    version 2 add DB

---
+++ db/__init__.py
@@ -0,0 +1,2 @@
+from db.models import User
+from db.base import db as database, CreatedModel
---
+++ db/models/users.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from sqlalchemy import ForeignKey, BigInteger, VARCHAR, Float
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+from db.base import CreatedModel
+
+
+class User(CreatedModel):
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    first_name: Mapped[str] = mapped_column(VARCHAR(255))
+    last_name: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
+    username: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger)
+    phone_number: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
+    products: Mapped[List["Product"]] = relationship(back_populates="user", cascade="all, delete")
+
+
+class Category(CreatedModel):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(VARCHAR(20))
+    products: Mapped[List["Product"]] = relationship(back_populates="category")
+
+
+class Product(CreatedModel):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    price: Mapped[str] = mapped_column(Float())
+    name: Mapped[str] = mapped_column(VARCHAR(20))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped[List["User"]] = relationship(back_populates="products")
+    category_id: Mapped[int] = mapped_column(ForeignKey("categorys.id"))
+    category: Mapped[List["Category"]] = relationship(back_populates="products", cascade="all, delete")
